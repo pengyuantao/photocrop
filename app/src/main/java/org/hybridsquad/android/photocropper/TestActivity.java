f09e3643d761e5c00e190bab0417b312a6e465f7
penggit <pengyuantao5354@126.com>
2016-02-15 18:46:19 +0800

    首次提交

---
+++ app/src/main/java/org/hybridsquad/android/photocropper/TestActivity.java
@@ -0,0 +1,126 @@
+package org.hybridsquad.android.photocropper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import org.hybridsquad.android.library.CropCallback;
+import org.hybridsquad.android.library.CropHelper;
+import org.hybridsquad.android.library.CropParams;
+
+
+public class TestActivity extends AppCompatActivity implements CropCallback, View.OnClickListener {
+
+    public static final String TAG = "TestActivity";
+
+    ImageView mImageView;
+
+    CropParams mCropParams;
+    private CropHelper mCropHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+        mCropParams = new CropParams(this);
+        mImageView = (ImageView) findViewById(R.id.image);
+        mCropHelper = CropHelper.getInstance(this);
+        mCropParams.setAspect(120,57);
+        mCropParams.setOutput(600,285);
+
+        findViewById(R.id.bt_crop_capture).setOnClickListener(this);
+        findViewById(R.id.bt_crop_gallery).setOnClickListener(this);
+        findViewById(R.id.bt_capture).setOnClickListener(this);
+        findViewById(R.id.bt_gallery).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.bt_crop_capture: {
+                mCropParams.setCompress(false);
+                mCropHelper.startCameraCropIntent();
+            }
+            break;
+            case R.id.bt_crop_gallery: {
+                mCropParams.setCompress(false);
+                mCropHelper.startGalleryCropIntent();
+            }
+            break;
+            case R.id.bt_capture: {
+                mCropParams.setCompress(false);
+                mCropHelper.startCameraIntent();
+
+            }
+            break;
+            case R.id.bt_gallery: {
+                mCropParams.setCompress(true);
+                mCropHelper.startGalleryIntent();
+            }
+            break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //交给代理方法去处理
+        mCropHelper.handleResult( requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCropHelper.clearCacheDir();
+    }
+
+    @Override
+    public CropParams getCropParams() {
+        return mCropParams;
+    }
+
+    @Override
+    public void onPhotoCropped(Uri uri) {
+//        Picasso.with(this).load(uri).resize(mImageView.getMeasuredWidth(),mImageView.getMeasuredHeight()).centerInside().skipMemoryCache().into(mImageView);
+        Log.i(TAG, "Crop Uri in path: " + uri.getPath());
+        mCropHelper.display(mImageView,mCropParams,uri);
+
+    }
+
+    @Override
+    public void onPhotoSelect(Uri uri) {
+        Log.i(TAG, "Select Uri in path: " + uri.getPath());
+        mCropHelper.display(mImageView, mCropParams, uri);
+//        Picasso.with(this).load(uri).resize(mImageView.getMeasuredWidth(),mImageView.getMeasuredHeight()).centerInside().skipMemoryCache().into(mImageView);
+    }
+
+    @Override
+    public void onCompressed(Uri uri) {
+        Log.i(TAG, "Compress Uri in path: " + uri.getPath());
+        mCropHelper.display(mImageView, mCropParams, uri);
+//        Picasso.with(this).load(uri).resize(mImageView.getMeasuredWidth(),mImageView.getMeasuredHeight()).centerInside().skipMemoryCache().into(mImageView);
+    }
+
+    @Override
+    public void onCancel(int requestCode) {
+        Toast.makeText(this, "取消了操作!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFailed(String message) {
+        Toast.makeText(this, "操作失败: " + message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void handleIntent(Intent intent, int requestCode) {
+        startActivityForResult(intent, requestCode);
+    }
+}
---
+++ photocrop/src/main/java/org/hybridsquad/android/library/CropCallback.java
@@ -0,0 +1,32 @@
+package org.hybridsquad.android.library;
+
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@xisue.com
+ * Date: 10/1/14
+ * Time: 11:00 AM
+ * Desc: CropCallback
+ * Revision:
+ * - 10:20 2014/10/01 The basic interfaces.
+ * - 13:00 2014/10/03 Able to get access to the CropParams the related Context.
+ * - 23:00 2015/09/05 Remove getContext, rename onCancel, onFailed, add handleIntent.
+ */
+public interface CropCallback {
+
+    void onPhotoCropped(Uri uri);//图片裁剪
+
+    void onPhotoSelect(Uri uri);//图片选择
+
+    void onCompressed(Uri uri);//图片压缩
+
+    void onCancel(int requestCode);//取消操作
+
+    void onFailed(String message);//失败操作
+
+    void handleIntent(Intent intent, int requestCode);//进行startActivityForResult
+
+    CropParams getCropParams();//获取裁剪参数
+}
